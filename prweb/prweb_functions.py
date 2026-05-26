073cbb95b3667fa4b12d247c3fef5c84a847419b
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2026-05-26 14:49:33 -0300

    fix:Existia um trecho que aumentava muito o tempo de espera

--- prweb/prweb_functions.py
+++ prweb/prweb_functions.py
@@ -1300,8 +1300,6 @@ def boxiamento_carga(page,
                     page.wait_for_timeout(500)
 
                     page.once("dialog", handle_dialog)
-                    
-                    page.wait_for_timeout(20000)
 
                     page.locator('xpath=//*[@id="NM_BOT_LIM"]').click()
                     page.wait_for_timeout(500)
