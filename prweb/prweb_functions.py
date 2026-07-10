5b4678c6eed7041cfdcaf979bb0e3f037c0e9610
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2026-07-10 16:00:39 -0300

    Removendo PE do boxiamento

--- prweb/prweb_functions.py
+++ prweb/prweb_functions.py
@@ -1238,6 +1238,11 @@ def boxiamento_carga(page,
 
                     box_resolvido = None
 
+
+                    if "PE" in xpath_estado_carga:
+                        box = "921"
+                        xpath_valor_box.fill(box)
+                        logger.info(f'Box PE {box}')
                     # Boxiamento priorizando regras com rota e match em contrato
                     box_resolvido = _resolve_box_for_carga(
                         cargas_box_map=cargas_box_map,
